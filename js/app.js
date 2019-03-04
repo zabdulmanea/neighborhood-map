55496060b0d1f89f4e79b46bc43a92a602295b56
Zainab-Abdulmanea <zezo.saz@gmail.com>
2019-03-04 13:06:41 +0300

    Update data Model attributes

--- js/app.js
+++ js/app.js
@@ -1,41 +1,36 @@
-/* Data Model: Includes a list of my favorite locations in Al-Nuzhah Neighborhood at Jeddah */
+/* ======= MODEL ======= */
+/* Includes a list of my favorite locations in Al-Nuzhah Neighborhood at Jeddah */
 var Model = {
-    // currentMarker: ko.observable(null),
     markers: [
         {
             title: 'Mall of Arabia',
             location: { lat: 21.6324851, lng: 39.1538997 },
-            type: 'Shopping Mall'
         },
 
         {
-            title: 'Alam Al Wesam',
-            location: { lat: 21.6258524, lng: 39.1600955 },
-            type: 'Discount store'
+            title: 'Jawad Center',
+            location: { lat: 21.626197, lng: 39.1599411 },
         },
 
         {
             title: 'Rotana Hall Wedding',
             location: { lat: 21.6291716, lng: 39.1548892 },
-            type: 'Wedding Venue'
         },
 
         {
             title: 'Makkah Restaurant',
             location: { lat: 21.6329237, lng: 39.166206 },
-            type: 'Pakistani restaurant'
         },
 
         {
             title: 'AlBaik',
             location: { lat: 21.6170575, lng: 39.1633406 },
-            type: 'Fast food restaurant'
         }
     ]
 };
-
-/* ViewModel */
-// var ViewModel = function () {};
+/* ======= END MODEL ======= */
 
 
-// ko.applyBindings(new ViewModel());
+
+/* ======= MAIN ======= */
+ko.applyBindings(new ViewModel());
